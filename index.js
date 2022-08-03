eefcb1d085da531c79caee2dd8a889cca968163c
NatalyKokidko <108397252+NatalyKokidko@users.noreply.github.com>
2022-08-03 20:07:08 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var userName = 'Kokidko Natalya'; // string
+var age = 41; // number
+var bool = true;
+
+var array = [ 'Natalya', 41, true ];
+var array2 = [ userName, age ]; // camelCase
+
+
+alert('Kokidko Natalya: ' + age);
