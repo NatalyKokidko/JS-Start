367328c7cc548a804f6aa7b2f4078c55d183d7c8
unknown <you@example.comnkokodko25@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[i[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D>
2022-08-10 18:31:45 +0200

    Added file with math max

---
+++ text.js
@@ -0,0 +1,5 @@
+let a = prompt ('Enter the first number','');
+let b = prompt ('Enter  the second number','');
+let c = prompt ('Enter the third number','');
+let result = Math.max(+a, +b, +c);
+alert (result); 
