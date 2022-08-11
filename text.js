cc41f5b2a97a2ec47be975a3b7a94dbc5db97192
unknown <you@example.comnkokodko25@gmail.com[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[i[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D>
2022-08-11 12:02:03 +0200

    change let/var

--- text.js
+++ text.js
@@ -1,5 +1,5 @@
-let a = prompt ('Enter the first number','');
-let b = prompt ('Enter  the second number','');
-let c = prompt ('Enter the third number','');
-let result = Math.max(+a, +b, +c);
+var a = prompt ('Enter the first number','');
+var b = prompt ('Enter  the second number','');
+var c = prompt ('Enter the third number','');
+var result = Math.max(+a, +b, +c);
 alert (result); 
