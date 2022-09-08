4ad709faf364f3768fb7a031f07bf6bb9aef63a8
Natalya <108397252+NatalyKokidko@users.noreply.github.com>
2022-09-08 17:36:11 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-var userName = 'Kokidko Natalya'; // string
-var age = 41; // number
-var bool = true;
+function isATheLargest(a, b, c) {
+  if (a > b && a > c) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-var array = [ 'Natalya', 41, true ];
-var array2 = [ userName, age ]; // camelCase
+function isBTheLargest(a, b, c) {
+  if (b > a && b > c) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
+function isCTheLargest(a, b, c) {
+  if (c > a && c > b) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-alert('Kokidko Natalya: ' + age);
+export function TheLargestNumber(a1, b1, c1) {
+  let a = parseFloat(a1);
+  let b = parseFloat(b1);
+  let c = parseFloat(c1);
+
+  if (isATheLargest(a, b, c)) {
+    return "The 1st number is largest and equal: " + a;
+  } else if (isBTheLargest(a, b, c)) {
+    return "The 2nd number is largest and equal: " + b;
+  } else if (isCTheLargest(a, b, c)) {
+    return "The 3rd number is largest and equal: " + c;
+  } else {
+    return "All three numbers are equal. And equal: " + a;
+  }
+}
